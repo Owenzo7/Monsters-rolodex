c86898543911e045f50fe3c1a24f7d35efca52c3
Owenzo7 <owenrogers545@gmail.com>
2023-02-24 14:34:26 +0300

    Learnt about Component State

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  constructor (){
+
+    super();
+
+    this.state = {
+
+      name:'Lee',
+    }
+  }
 
   render(){
     return (
@@ -10,16 +20,11 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello my name Is Owenzo.
+            Hi {this.state.name}.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button>
+          Change Name.
+          </button>
         </header>
       </div>
     );
