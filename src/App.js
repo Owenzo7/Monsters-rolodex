3ba5f4f842b34feb434b8d4de75492ea3058fa47
Owenzo7 <owenrogers545@gmail.com>
2023-02-25 14:49:20 +0300

    Learning about State and SetState

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  
+
   constructor(){
-    super()
 
+    super();
 
-    this.state = {
-      name: "Jobs"
+    this.state={
+      name:{firstName: 'Jacob', lastName: 'Juma'},
+      company:"Safaricom"
     }
   }
+  
+
 
   render(){
     return (
@@ -19,10 +22,11 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi {this.state.name}.
+            Hi my name is {this.state.name.firstName} {this.state.name.lastName} I work at {this.state.company}.
           </p>
           <button onClick={() => {
-            this.setState({name:'James'})
+            this.setState({name:{firstName:"Lisa", lastName:"Kimathi"}})
+            console.log(this.state)
           }}>
           Change Name.
           </button>
