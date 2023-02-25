7abffe005da641deeec36375797c49381e758a03
Owenzo7 <owenrogers545@gmail.com>
2023-02-25 15:17:40 +0300

    used setState with a Callback function

--- src/App.js
+++ src/App.js
@@ -1,40 +1,45 @@
-import { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
-
-  constructor(){
-
+  constructor() {
     super();
 
-    this.state={
-      name:{firstName: 'Jacob', lastName: 'Juma'},
-      company:"Safaricom"
-    }
+    this.state = {
+      name: { firstName: "Juma", lastName: "Anderson" },
+      company: "ZTM",
+    };
   }
-  
 
-
-  render(){
+  render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi my name is {this.state.name.firstName} {this.state.name.lastName} I work at {this.state.company}.
+            Hi my name is {this.state.name.firstName} {this.state.name.lastName}{" "}
+            and i work for {this.state.company}
           </p>
-          <button onClick={() => {
-            this.setState({name:{firstName:"Lisa", lastName:"Kimathi"}})
-            console.log(this.state)
-          }}>
-          Change Name.
+          <button
+            onClick={() => {
+              this.setState(
+                () => {
+                  return {
+                    name: { firstName: "Lisa", lastName: "Monroe" },
+                    company: "IBM",
+                  };
+                },
+                () => console.log(this.state)
+              );
+            }}
+          >
+            Change Name.
           </button>
         </header>
       </div>
     );
   }
-  
 }
 
 export default App;
