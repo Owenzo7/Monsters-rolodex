52d88de415905cea5c7fd4671af7550819b196c5
Owenzo7 <owenlee545@gmail.com>
2023-03-25 15:56:43 +0300

    done with changing to functional components for the day

--- src/components/Cardlist/card-list.component.jsx
+++ src/components/Cardlist/card-list.component.jsx
@@ -1,16 +1,13 @@
-import React, { Component } from "react";
 import Card from "../Card/card.component";
 import "./card-list.styles.css";
 
-export default class Cardlist extends Component {
-  render() {
-    const { monsters } = this.props;
-    return (
-      <div className="card-list">
-        {monsters.map((monster) => {
-          return <Card monster={monster} />;
-        })}
-      </div>
-    );
-  }
-}
+const Cardlist = ({monsters}) =>  (
+    <div className="card-list">
+      {monsters.map((monster) => {
+        return <Card monster={monster} />;
+      })}
+    </div>
+  );
+
+
+export default Cardlist;
--- src/components/Searchbox/search-box.component.jsx
+++ src/components/Searchbox/search-box.component.jsx
@@ -1,15 +1,14 @@
-import React, { Component } from "react";
 import "./search-box.styles.css";
 
-export default class searchBox extends Component {
-  render() {
-    return (
-      <input
-        className={` search-box ${this.props.className}`}
-        type="search"
-        placeholder={this.props.placeholder}
-        onChange={this.props.onChangeHandler}
-      />
-    );
-  }
-}
+const searchBox = ({ className, placeholder, onChangeHandler }) => {
+  return (
+    <input
+      className={` search-box ${className}`}
+      type="search"
+      placeholder={placeholder}
+      onChange={onChangeHandler}
+    />
+  );
+};
+
+export default searchBox;
